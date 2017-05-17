21973b96d5118696a4c9b695d7901005646f32ef
leeshine <lichgu@aliyun.com>
2017-05-17 16:39:38 +0800

    Create StringFind.c

---
+++ StringFind.c
@@ -0,0 +1,19 @@
+static int StringFind(const char *pSrc, const char *pDst)  
+{  
+    int i, j;  
+    for (i=0; pSrc[i]!='\0'; i++)  
+    {  
+        if(pSrc[i]!=pDst[0])  
+            continue;         
+        j = 0;  
+        while(pDst[j]!='\0' && pSrc[i+j]!='\0')  
+        {  
+            j++;  
+            if(pDst[j]!=pSrc[i+j])  
+            break;  
+        }  
+        if(pDst[j]=='\0')  
+            return i;  
+    }  
+    return -1;  
+}  
